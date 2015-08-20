ad26fdc56335f5ad886c1e6e5ad006261e2afe33
DanielSandoval <danielsandoval182@gmail.com>
2015-08-20 16:44:44 -0600

    Trying to make the board

--- application.py
+++ application.py
@@ -2,13 +2,32 @@
 
 import os
 import sys
+import random
 
 class my_game(object):
 	"""docstring for ClassName"""
 	def __init__(self):
-		pass
+		self.board = []
 
 	def menu(self):
 		menu_option = {}
 
-user1 = my_game
+	def my_board(self):
+		self.board.append(['	'] * 5)
+		for raw in self.board:
+			for column in self.board:
+				print "|".join(column)
+			print "----".join(raw)
+
+	def random_column(self):
+		return random.randint(0, len(self.board) -1)
+
+	def random_row(self):
+		return random.randint(0, len(self.board) -1)
+
+user1 = my_game()
+user1.my_board()
+user1.my_board()
+user1.my_board()
+#print user1.random_column()
+#print user1.random_row()
